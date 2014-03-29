4a7efb09e89955ad335b3d4e26f7fdaca759112f
warreee <schodtsward@gmail.com>
2014-03-30 00:05:17 +0100

    Oefening 5 selecteer reisbureau

--- luchtvaart/boeking_selecteerreisbureau.php
+++ luchtvaart/boeking_selecteerreisbureau.php
@@ -14,7 +14,18 @@
 <form action="boeking_selecteerklant.php">
 
 <!-- Voeg hier je code toe -->
+<p><em>Reisbureau:</em>
+<select name="naam">
+<?php
+	$query = "SELECT Reisbureau_ID, Straat, Nummer FROM Reisbureau ORDER BY Reisbureau_ID";
+	$resultaat = mysql_query($query) or die("Kan de lijst van reisbureaus niet opvragen: " . mysql_error());
+	while($rij = mysql_fetch_array($resultaat)) {
+		echo "<option value=\"". $rij['Reisbureau_ID']  . "\">" .$rij['Reisbureau_ID'] . " " . $rij['Straat'] . " " .$rij['Nummer'] . "</option>";
+	}
+?>
+</select>
 
+<input type="submit" value="Selecteer Reisbureau"/>
 </form>
 
 <?php
